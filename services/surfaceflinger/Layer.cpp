4ce2db603e8c4c257289dd5f548c51ed3f29d3bb
Colin Cross <ccross@android.com>
2016-09-26 18:10:16 -0700

    Rename dump methods to dumpState
    The dump methods shadow the IBinder dump method.  The warnings were being
    hidden by the use of -isystem to include frameworks/native/include.
    
    Bug: 31752268
    Test: m -j
    Merged-in: Iafc64da43032d5d9d84b64640e70d93fd7051bcf
    Change-Id: Iafc64da43032d5d9d84b64640e70d93fd7051bcf

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -2355,7 +2355,7 @@ void Layer::dump(String8& result, Colorizer& colorizer) const
             mQueuedFrames, mRefreshPending);
 
     if (mSurfaceFlingerConsumer != 0) {
-        mSurfaceFlingerConsumer->dump(result, "            ");
+        mSurfaceFlingerConsumer->dumpState(result, "            ");
     }
 }
 
